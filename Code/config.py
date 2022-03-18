82238155df0a8c287948b4236b48d2cd711d3b04
Tianming-Zhu <45647121+Tianming-Zhu@users.noreply.github.com>
2022-03-18 13:39:19 +0800

    Update config.py

--- Code/config.py
+++ Code/config.py
@@ -2,33 +2,6 @@
 # It can be updated using argparse as well.
 
 
-
-class ctgan_setting:
-    EMBEDDING = 128
-    DEPTH = 2
-    WIDTH = 256
-    GENERATOR_LEARNING_RATE = 2e-4
-    DISCRIMINATOR_LEARNING_RATE = 2e-5
-    BATCH_SIZE = 500
-    EPOCHS = 2 # 800
-    DROPOUT = 0.5
-    DISCRIMINATOR_STEP = 1
-    CONDGEN = True
-    DEVICE = "cpu"  # "cuda:0"
-
-
-class tvae_setting:
-    EMBEDDING = 128
-    DEPTH = 2
-    WIDTH = 128
-    LEARNING_RATE = 1e-3
-    BATCH_SIZE = 500
-    EPOCHS = 20 # 300
-    CONDGEN_ENCODER = True
-    CONDGEN_LATENT = True
-    DEVICE = "cpu"  # "cuda:0"
-
-
 class AE_setting:
     EMBEDDING = 128
     NUM_CHANNELS = 2
