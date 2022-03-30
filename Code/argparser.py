751b2e01445b06b8c3715c568ccb87353ceda31d
Tianming-Zhu <u0903411@gmail.com>
2022-03-30 13:18:25 +0800

    Updated

--- Code/argparser.py
+++ Code/argparser.py
@@ -46,23 +46,29 @@ def _parse_args():
     parser.add_argument('--AE_batchsize', default=None, type=int, metavar='', help='Autoencoder batch size')
     parser.add_argument('--AE_epochs', default=None, type=int, metavar='', help='Autoencoder num epochs')
     parser.add_argument('--AE_dropout', default=None, type=float, metavar='', help='Autoencoder dropout rate')
-    parser.add_argument('--AE_dis_step', default=None, type=int, metavar='', help='Autoencoder discriminator step')
     parser.add_argument('--AE_device', default=None, type=str, metavar='', help='Autoencoder cpu or cuda')
     parser.add_argument('--AE_metric', default=None, type=str, metavar='', help='Metric used for measuring reconstructed error')
 
 
 
     # CVAE parameters
-    parser.add_argument('--tv_embedding', default=None, type=int, metavar='', help='tvae embedding')
-    # parser.add_argument('--tv_condgen', default=None, type=str2bool, metavar='', help='tvae cond. gen.')
-    parser.add_argument('--tv_depth', default=None, type=int, metavar='', help='tvae num hidden layers')
-    parser.add_argument('--tv_width', default=None, type=int, metavar='', help='tvae width of mlp')
-    parser.add_argument('--tv_lr', default=None, type=float, metavar='', help='tvae learning rate')
-    parser.add_argument('--tv_batchsize', default=None, type=int, metavar='', help='tvae batch size')
-    parser.add_argument('--tv_epochs', default=None, type=int, metavar='', help='tvae num epochs')
-    parser.add_argument('--tv_device', default=None, type=str, metavar='', help='tvae cpu or cuda')
-    parser.add_argument('--tv_condgen_encoder', default=None, type=str2bool, metavar='', help='tvae cond. gen. to encoder')
-    parser.add_argument('--tv_condgen_latent', default=None, type=str2bool, metavar='', help='tvae cond. gen. to latent space')
+    parser.add_argument('--CVAE_embedding', default=None, type=int, metavar='', help='CVAutoencoder embedding')
+    parser.add_argument('--CVAE_num_channels', default=None, type=int, metavar='',
+                        help='CVAutoencoder number of channels')
+    parser.add_argument('--CVAE_stride', default=None, type=float, metavar='', help='CVAutoencoder stride')
+    parser.add_argument('--CVAE_kernel_size', default=None, type=float, metavar='',
+                        help='CVAutoencoder kernel size')
+    parser.add_argument('--CVAE_scale_factor', default=None, type=float, metavar='',
+                        help='CVAutoencoder scale factor')
+    parser.add_argument('--CVAE_lr', default=None, type=float, metavar='', help='CVAutoencoder learning rate')
+    parser.add_argument('--CVAE_batchsize', default=None, type=int, metavar='', help='CVAutoencoder batch size')
+    parser.add_argument('--CVAE_epochs', default=None, type=int, metavar='', help='CVAutoencoder num epochs')
+    parser.add_argument('--CVAE_dropout', default=None, type=float, metavar='', help='CVAutoencoder dropout rate')
+    parser.add_argument('--CVAE_device', default=None, type=str, metavar='', help='CVAutoencoder cpu or cuda')
+    parser.add_argument('--CVAE_metric', default=None, type=str, metavar='',
+                        help='Metric used for measuring reconstructed error')
+    parser.add_argument('--CVAE_loss_factor', default=None, type=str, metavar='',
+                        help='Weight for reconstructed error in loss function')
 
     return parser.parse_args()
 
@@ -167,9 +173,6 @@ class ParserOutput:
             if args.AE_dropout is not None:
                 cfg.AE_setting.DROPOUT = args.AE_dropout
 
-            if args.AE_dis_step is not None:
-                cfg.AE_setting.DISCRIMINATOR_STEP = args.AE_dis_step
-
             if args.AE_device is not None:
                 cfg.AE_setting.DEVICE = args.AE_device
 
@@ -179,31 +182,43 @@ class ParserOutput:
 
 
 
-        elif self.model_type == 'tablegan':
+        elif self.model_type == 'cvae':
+            if args.CVAE_embedding is not None:
+                cfg.CVAE_setting.EMBEDDING = args.CVAE_embedding
 
-            if args.tbl_embedding is not None:
-                cfg.tablegan_setting.EMBEDDING = args.tbl_embedding
+            if args.CVAE_num_channels is not None:
+                cfg.CVAE_setting.NUM_CHANNELS = args.CVAE_num_channels
 
-            if args.tbl_num_channels is not None:
-                cfg.tablegan_setting.NUM_CHANNELS = args.tbl_num_channels
+            if args.CVAE_stride is not None:
+                cfg.CVAE_setting.STRIDE = args.CVAE_stride
 
-            if args.tbl_dlayer is not None:
-                cfg.tablegan_setting.DLAYER = args.tbl_dlayer
+            if args.CVAE_kernel_size is not None:
+                cfg.CVAE_setting.KERNEL_SIZE = args.CVAE_kernel_size
 
-            if args.tbl_lr is not None:
-                cfg.tablegan_setting.LEARNING_RATE = args.tbl_lr
+            if args.CVAE_scale_factor is not None:
+                cfg.CVAE_setting.SCALE_FACTOR = args.CVAE_scale_factor
 
-            if args.tbl_batchsize is not None:
-                cfg.tablegan_setting.BATCH_SIZE = args.tbl_batchsize
+            if args.CVAE_lr is not None:
+                cfg.CVAE_setting.LEARNING_RATE = args.CVAE_lr
 
-            if args.tbl_epochs is not None:
-                cfg.tablegan_setting.EPOCHS = args.tbl_epochs
+            if args.CVAE_batchsize is not None:
+                cfg.CVAE_setting.BATCH_SIZE = args.CVAE_batchsize
 
-            if args.tbl_dis_step is not None:
-                cfg.tablegan_setting.DISCRIMINATOR_STEP = args.tbl_dis_step
+            if args.CVAE_epochs is not None:
+                cfg.CVAE_setting.EPOCHS = args.CVAE_epochs
 
-            if args.tbl_device is not None:
-                cfg.tablegan_setting.DEVICE = args.tbl_device
+            if args.CVAE_dropout is not None:
+                cfg.CVAE_setting.DROPOUT = args.CVAE_dropout
+
+            if args.CVAE_device is not None:
+                cfg.CVAE_setting.DEVICE = args.CVAE_device
+
+            if args.CVAE_metric is not None:
+                cfg.CVAE_setting.LOSS = args.CVAE_metric
+
+            if args.CVAE_loss_factor is not None:
+                cfg.CVAE_setting.LOSS_FACTOR = args.CVAE_loss_factor
+
 
         else:
             print('Please specify the correct model type.')
