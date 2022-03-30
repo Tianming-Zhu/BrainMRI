e970a04360f4dd5d48fab6cc7528f2af7b5ab181
Tianming-Zhu <u0903411@gmail.com>
2022-03-30 13:13:06 +0800

    Updated

--- Code/config.py
+++ Code/config.py
@@ -11,10 +11,20 @@ class AE_setting:
     LEARNING_RATE = 2e-4
     BATCH_SIZE = 50
     EPOCHS = 2 # 300
-    DISCRIMINATOR_STEP = 1
     DEVICE = "cpu"  # "cuda:0"
     DROPOUT = 0.5
     LOSS = 'l1'
- 
-       
-     
+
+class CVAE_setting:
+    EMBEDDING = 128
+    NUM_CHANNELS = 2
+    STRIDE = 2  # This is fixed
+    KERNEL_SIZE = 4  # This is fixed
+    SCALE_FACTOR = 2  # This is fixed
+    LEARNING_RATE = 2e-4
+    BATCH_SIZE = 50
+    EPOCHS = 2  # 300
+    DEVICE = "cpu"  # "cuda:0"
+    DROPOUT = 0.5
+    LOSS = 'l1'
+    LOSS_FACTOR = 1
